19c52c593da7e83b5a7047a1bc35f66c13f5e21f
muabdi <91607604+TheBetterDark@users.noreply.github.com>
2024-07-31 15:22:37 +0100

    Initial Commit

---
+++ philo/includes/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/31 15:09:24 by muabdi            #+#    #+#             */
+/*   Updated: 2024/07/31 15:14:41 by muabdi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+// Standard library headers
+
+# include <stdlib.h>
+
+// External Libraries
+
+// Struct declarations
+
+// Function declarations
+
+#endif
